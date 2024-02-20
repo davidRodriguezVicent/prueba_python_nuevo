c9651f2f5e5bc42944324d0230613262b074431f
CTAG\davrod <david.rodriguez@ctag.com>
2024-02-20 11:33:40 +0100

    input

---
+++ 1_basicos/carga.py
@@ -0,0 +1,9 @@
+print("hola")
+texto_largo="""
+1 rojo
+2 azul
+3 verde
+
+"""
+color=input("seleccionar tipo de color:"+texto_largo)
+print=(color)
--- pruebapy.py
+++ pruebapy.py
@@ -1,3 +1,4 @@
 x=2*6
-print "hola"
+print ("hola")
+
 
